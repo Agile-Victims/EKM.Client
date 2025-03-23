3807a951185df7cf82c51d8d5ff71dae1de660cb
Ghurstird <tanerciyas_2003@hotmail.com>
2025-03-23 23:48:25 +0300

    Design fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { MainPageComponent } from './features/common/pages/main-page/main-page.component';
 
-export const routes: Routes = [
-    { path: '', component: MainPageComponent }
-];
+export const routes: Routes = [];
