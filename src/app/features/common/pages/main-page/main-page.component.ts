013d999228fd9a6cb7fd09b875b04d1e29ede353
Ghurstird <tanerciyas_2003@hotmail.com>
2025-03-23 22:56:10 +0300

    main-page added, logo updated

---
+++ src/app/features/common/pages/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [NgOptimizedImage],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+
+}
