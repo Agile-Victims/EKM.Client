6ace921ec2fbec17726d9a87dbc34a5e72ad3f3d
Ghurstird <tanerciyas_2003@hotmail.com>
2025-04-21 20:58:34 +0300

    Fix and grant new css to Admin Main Page

--- src/app/features/admin/pages/admin-main-page/admin-main-page.component.ts
+++ src/app/features/admin/pages/admin-main-page/admin-main-page.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-admin-main-page',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './admin-main-page.component.html',
   styleUrl: './admin-main-page.component.css'
 })
