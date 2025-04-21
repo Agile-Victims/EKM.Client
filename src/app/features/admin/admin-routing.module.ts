27f9811aa033abe4ec13659efe4394020db3ad32
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-04-21 19:45:50 +0300

    exams-page and router added

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLoginPageComponent } from './pages/admin-login-page/admin-login-page.component';
 import { AdminMainPageComponent } from './pages/admin-main-page/admin-main-page.component';
+import { ExamsPageComponent } from './pages/exams-page/exams-page.component';
 
 const routes: Routes = [
+  { path: '', component: AdminMainPageComponent },
   { path: 'login', component: AdminLoginPageComponent },
-  { path: '', component: AdminMainPageComponent}
+  { path: 'exams', component: ExamsPageComponent }
 ];
 
 @NgModule({
