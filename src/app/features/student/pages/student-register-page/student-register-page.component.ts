5faecd419dc4f71c757574fb31e987e1ecf5270b
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-04-28 12:42:32 +0300

    cleaning

--- src/app/features/student/pages/student-register-page/student-register-page.component.ts
+++ src/app/features/student/pages/student-register-page/student-register-page.component.ts
@@ -27,34 +27,3 @@ export class StudentRegisterPageComponent {
   ).subscribe();
   }
 }
-
-
-// import { Component } from '@angular/core';
-// import { RegisterComponent } from 'path-to-register-component/register.component';
-// import { StudentRegisterService } from '../../services/student-register.service';
-// import { RegisterRequest } from 'path-to-models/register-request.model';
-
-// @Component({
-//   selector: 'app-student-register-page',
-//   standalone: true,
-//   imports: [RegisterComponent],
-//   templateUrl: './student-register-page.component.html',
-//   styleUrls: ['./student-register-page.component.css']
-// })
-// export class StudentRegisterPageComponent {
-
-//   constructor(private registerService: StudentRegisterService) {}
-
-//   handleRegister(credentials: { firstName: string; lastName: string; email: string; password: string; confirmPassword: string }): void {
-//     // Create a registration request (typically, you might not send the confirmPassword)
-//     const registerRequest = new RegisterRequest(credentials.firstName, credentials.lastName, credentials.email, credentials.password);
-//     this.registerService.register(registerRequest).subscribe(
-//       (response) => {
-//         window.alert(`Kayıt başarılı: ${response}`);
-//       },
-//       (error) => {
-//         window.alert(`Kayıt başarısız`);
-//       }
-//     );
-//   }
-// }
