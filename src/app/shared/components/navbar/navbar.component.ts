c14c73580370f4f00ffbb55e264def08ef68aba6
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-05-06 12:52:41 +0300

    logout added

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,14 +1,35 @@
-import { NgOptimizedImage } from '@angular/common';
+import { NgIf, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, NgOptimizedImage],
+  imports: [RouterLink, NgOptimizedImage, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  currentLanguage: string = '';
+  isLoggedIn: boolean = false;
+  isDropdownVisible: boolean = false;
 
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authService.loggedIn$.subscribe((status: boolean) => {
+      this.isLoggedIn = status;
+      console.log(this.isLoggedIn)
+    });
+  }
+
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+    this.toggleDropdown();
+  }
 }
